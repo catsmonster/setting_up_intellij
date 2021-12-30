d87c2c3a6b7fde214e9295e75552e584796e9dd3
catsmonster <shaked.brand@gmail.com>
2021-12-30 20:43:10 +0200

    click here

---
+++ src/ClickHere.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class ClickHere {
+    public static void main(String[] args) {
+        System.out.println("Click here to find out more:\nhttps://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+
+}
